4f637f0e6f6c645fc359d8af9c3e623efa0d64f5
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-12-06 08:43:53 -0600

    added sections on drilling into the DOM

--- days-33-37-DOM-MANIPULATION/day33/index.js
+++ days-33-37-DOM-MANIPULATION/day33/index.js
@@ -1,3 +1,11 @@
 //console.dir(document);
 
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
+
+let anchElement = document.getElementById('external-link');
+
+anchElement.href = 'https://yahoo.com';
+
+// anchElement = document.querySelector('#external-link');
+
+// anchElement = 'https://udemy.com'
